c00b94d08037223da128536053cb73145c555d38
notDecay <thisisjustalongtext@gmail.com>
2023-01-02 19:20:15 +0700

    Better documentation

--- main.js
+++ main.js
@@ -1,49 +1,60 @@
 const APP_VERSION = '1.0.0 dev'
-
+/**This is the main entry point of the app
+ * @returns {Promise<void>}
+ * @event   `window.onload`
+ */
 async function main() {
   log('app version '+APP_VERSION)
+  // get all of the todo elements
   const todoCompoment = getTodoElements()
-  if (todoCompoment === null) return
+  if (todoCompoment === undefined) return
   const { addTodoButton, todoInput, todoList } = todoCompoment
-  
-  /**@type {string[]} */
-  const arrayOfTodoData = []
+  // get all of the todos from localStorage
   const existingTodos = await getTodoData()
-  /**Add a new todo 
-   * @param {string} todoData
-   */
-  const addTodo = (todoData) => {
-    log('Todo mounted')
-    arrayOfTodoData.push(todoData)
-    // Create a new element
-    // <ul class="todos-list">
-    //   <li class="todo"></li>
-    // </ul>
-    const todo = document.createElement('li')
-    todo.className = 'todo'
-    todo.innerHTML = todoData
-    todoList.appendChild(todo) // make it appear to the screen
-    localStorage.setItem('todos', JSON.stringify(arrayOfTodoData)) // save it to the localStorage
-    todoInput.value = '' // reset the input value
-  }
-
-  existingTodos.forEach(todo => addTodo(todo))
-
-  // Events
+  /**@type {string[]} */ 
+  const arrayOfTodoData = []
+  // render all of the todo
+  existingTodos.forEach(todo => addTodo(todo, arrayOfTodoData, todoList, todoInput))
+  // add a new todo when you click the add todo button
   addTodoButton.onclick = (event) => {
     event.preventDefault()
-    addTodo(todoInput.value)
+    addTodo(todoInput.value, arrayOfTodoData, todoList, todoInput)
   }
 }
 
+/**Add a new todo 
+ * @param {string} todoData             the todo data
+ * @param {string[]} arrayOfTodoData    array of todo data that it's loaded from {@link localStorage}
+ * @param {Element} todoList            the todo element that used to append the todo
+ * @param {HTMLInputElement} todoInput  used to get the data from the input area, then append it to the `todoList`
+ */
+function addTodo(todoData, arrayOfTodoData, todoList, todoInput) {
+  log('Todo mounted')
+  arrayOfTodoData.push(todoData)
+  // Create a new element
+  // <ul class="todos-list">
+  //   <li class="todo"></li>
+  // </ul>
+  const todo = document.createElement('li')
+  todo.className = 'todo'
+  todo.innerHTML = todoData
+  todoList.appendChild(todo) // make it appear to the screen
+  localStorage.setItem('todos', JSON.stringify(arrayOfTodoData)) // save it to the localStorage
+  todoInput.value = '' // reset the input value
+}
+
+/**Clear all of the todo data from {@link localStorage}, this function
+ * is only for development
+ * @returns {void} nothing
+ */
 function clearCacheData() {
   localStorage.removeItem('todos')
   window.location.reload()
 }
 
-/**Get the todo data from {@link localStorage}
+/**Get the todo data from {@link localStorage} or empty array
+ * if there's no data
  * @returns {Promise<string[]>} a array of todo data
- * @async
  */
 async function getTodoData() {
   return new Promise((resolve) => {
@@ -56,7 +67,7 @@ async function getTodoData() {
  *   addTodoButton: HTMLButtonElement,
  *   todoInput: HTMLInputElement,
  *   todoList: Element
- * } | null} a object of elements or *nothing* if it could not find the elements
+ * } | undefined} a object of todo elements or `undefined` if it could not find one of the elements
  */
 function getTodoElements() {
   /**@type {HTMLButtonElement | null} */
@@ -64,15 +75,15 @@ function getTodoElements() {
   /**@type {HTMLInputElement | null} */
   const todoInput = document.querySelector("input[name='todo']")
   const todoList = document.querySelector('ul.todos-list')
-  if (addTodoButton === null) return null
-  if (todoList === null) return null
-  if (todoInput === null) return null
+  if (addTodoButton === null) return void console.error('failed to find the element')
+  if (todoList === null) return void console.error('failed to find the element')
+  if (todoInput === null) return void console.error('failed to find the element')
 
   return { addTodoButton, todoInput, todoList }
 }
 
 /**Just a log function
- * @param {*} message 
+ * @param {*} message anything
  */
 function log(message) {
   logInfo('Todo', '#ff8484ff', message)
